b5af4f862dbe8046566d81d44ff4b90623a485ce
arafataft <arafatcd@gmail.com>
2023-05-19 11:47:48 +0600

    login google

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,6 +4,8 @@ import { FaGoogle } from 'react-icons/fa';
 import { Container, Col, Card, Form, Button } from 'react-bootstrap';
 import { useContext, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
 
 
 const Login = () => {
@@ -13,7 +15,17 @@ const Login = () => {
   const location = useLocation();
   const from = location.state?.from?.pathname || '/';
 
-  
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then(() => {
+        navigate(from);
+        setError(null);
+      })
+      .catch(error => console.error(error.message))
+  }
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -23,19 +35,19 @@ const Login = () => {
     console.log(email, password);
 
     signIn(email, password)
-            .then( ()=> {
-                navigate(from);
-                setError('')
-            })
-            .catch(error => {
-                if (error.code === "auth/user-not-found") {
-                    setError("User not found");
-                } else if (error.code === "auth/wrong-password") {
-                    setError("Invalid password");
-                } else {
-                    setError(error.message);
-                }
-            })
+      .then(() => {
+        navigate(from);
+        setError('')
+      })
+      .catch(error => {
+        if (error.code === "auth/user-not-found") {
+          setError("User not found");
+        } else if (error.code === "auth/wrong-password") {
+          setError("Invalid password");
+        } else {
+          setError(error.message);
+        }
+      })
   };
 
   return (
@@ -56,12 +68,12 @@ const Login = () => {
 
               </Form.Group>
               <Form.Text className="text-danger">
-                    {setError&&<p>{Error}</p>}
-                </Form.Text>
+                {setError && <p>{Error}</p>}
+              </Form.Text>
               <Button variant="primary" type="submit" className="d-block mx-auto mt-3">Login</Button>
             </Form>
             <p className="text-center mt-3">New to LegoLand Toys? <Link to="/signup">Sign Up</Link></p>
-            <Button className='mb-2 mx-auto d-block' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
+            <Button onClick={handleGoogleLogin} className='mb-2 mx-auto d-block' variant="outline-primary"> <FaGoogle /> Login with Google</Button>
           </Card.Body>
         </Card>
       </Col>
