5ee1e10671a7225fbf4baf7bbc35e8feb822987b
arafataft <arafatcd@gmail.com>
2023-05-21 03:08:49 +0600

    ascending descendin by price my toy

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,8 +1,8 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import { Container, Table, Button, Modal, Image } from 'react-bootstrap';
 import useTitle from '../../Hook/useTitle';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 const MyToys = () => {
   const { user } = useContext(AuthContext);
@@ -12,6 +12,7 @@ const MyToys = () => {
   const [updatedPrice, setUpdatedPrice] = useState('');
   const [updatedQuantity, setUpdatedQuantity] = useState('');
   const [updatedDescription, setUpdatedDescription] = useState('');
+  const [sortAscending, setSortAscending] = useState(false); // State for sorting direction
 
   useTitle('My Toys');
 
@@ -28,13 +29,12 @@ const MyToys = () => {
       console.log('Error:', error);
     }
   }, [user]);
-  
+
   useEffect(() => {
     if (user) {
       fetchToys();
     }
   }, [user, fetchToys]);
-  
 
   const handleDeleteToy = async (id) => {
     if (window.confirm('Are you sure you want to delete this toy?')) {
@@ -94,60 +94,110 @@ const MyToys = () => {
     setUpdatedDescription('');
   };
 
+  const handleSort = () => {
+    setSortAscending(!sortAscending);
+    // Fetch toys again with the updated sort direction
+    fetchToys();
+  };
+
+  // Sort the toys based on the price and sort direction
+  const sortedToys = sortAscending ? toys.sort((a, b) => a.price - b.price) : toys.sort((a, b) => b.price - a.price);
+
   return (
-    <div>
+    <Container>
       <h1>My Toys</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Description</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {toys.map((toy) => (
-            <tr key={toy._id}>
-              <td>{toy.name}</td>
-              <td>{toy.price}</td>
-              <td>{toy.quantity}</td>
-              <td>{toy.description}</td>
-              <td>
-                <button onClick={() => handleDeleteToy(toy._id)}>Delete</button>
-                <button onClick={() => handleShowModal(toy)}>Update</button>
-              </td>
+      <div className="table-responsive">
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Seller Name</th>
+              <th>Seller Email</th>
+              <th>Sub-category</th>
+              <th>
+                Price{' '}
+                <Button variant="link" onClick={handleSort}>
+                  {sortAscending ? '⬆' : '⬇'} 
+                  {/* Here use up down symbol as button asc/dsc */}
+                </Button>
+              </th>
+              <th>Rating</th>
+              <th>Quantity</th>
+              <th>Description</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {sortedToys.map((toy) => (
+              <tr key={toy._id}>
+                <td>
+                  <Image src={toy.pictureUrl} alt="Toy" className="toy-image " height={95} />
+                </td>
+                <td>{toy.name}</td>
+                <td>{toy.sellerName}</td>
+                <td>{toy.sellerEmail}</td>
+                <td>{toy.subCategory}</td>
+                <td>${toy.price}</td>
+                <td>{toy.rating}</td>
+                <td>{toy.quantity}</td>
+                <td>{toy.description}</td>
+                <td>
+                  <Button variant="danger" className="me-1 mb-1" onClick={() => handleDeleteToy(toy._id)}>
+                    <FaTrash />
+                  </Button>
+                  <Button variant="primary" onClick={() => handleShowModal(toy)}>
+                    <FaEdit />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
 
       <Modal show={showModal} onHide={handleCloseModal}>
         <Modal.Header closeButton>
           <Modal.Title>Update Toy</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <label htmlFor="price">Price:</label>
-          <input
-            type="number"
-            id="price"
-            value={updatedPrice}
-            onChange={(e) => setUpdatedPrice(e.target.value)}
-          />
-          <label htmlFor="quantity">Quantity:</label>
-          <input
-            type="number"
-            id="quantity"
-            value={updatedQuantity}
-            onChange={(e) => setUpdatedQuantity(e.target.value)}
-          />
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            value={updatedDescription}
-            onChange={(e) => setUpdatedDescription(e.target.value)}
-          ></textarea>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="price" className="form-label">
+                Price:
+              </label>
+              <input
+                type="number"
+                id="price"
+                className="form-control"
+                value={updatedPrice}
+                onChange={(e) => setUpdatedPrice(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="quantity" className="form-label">
+                Quantity:
+              </label>
+              <input
+                type="number"
+                id="quantity"
+                className="form-control"
+                value={updatedQuantity}
+                onChange={(e) => setUpdatedQuantity(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="description" className="form-label">
+                Description:
+              </label>
+              <textarea
+                id="description"
+                className="form-control"
+                value={updatedDescription}
+                onChange={(e) => setUpdatedDescription(e.target.value)}
+              ></textarea>
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>
@@ -158,7 +208,7 @@ const MyToys = () => {
           </Button>
         </Modal.Footer>
       </Modal>
-    </div>
+    </Container>
   );
 };
 
