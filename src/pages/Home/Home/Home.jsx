811b4e6cacbe944466ed4f7d4fbb72d75a1b8e15
arafataft <arafatcd@gmail.com>
2023-05-20 14:09:05 +0600

    aos setup in banner and category

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,30 @@
+import { useEffect } from "react";
 import Banner from "../Banner/Banner";
 import ByCategory from "../ByCategory/ByCategory";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // animation duration
+            easing: 'ease-in-out', // animation easing
+            once: true // whether animation should execute only once
+        });
+    }, []);
+
+
     return (
-        <div>
-            <Banner/>
-            <ByCategory/>
-        </div>
+        <>
+            <div data-aos="fade-up">
+            <Banner />
+            </div>
+            <div data-aos="fade-up">
+            <ByCategory />
+            </div>
+            
+            
+        </>
     );
 };
 
