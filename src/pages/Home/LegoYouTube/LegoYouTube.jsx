dc39dc922888cb0cb268d4965874605d0679cc4b
arafataft <arafatcd@gmail.com>
2023-05-21 17:36:08 +0600

    make video responsive

--- src/pages/Home/LegoYouTube/LegoYouTube.jsx
+++ src/pages/Home/LegoYouTube/LegoYouTube.jsx
@@ -1,20 +1,23 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const LegoYouTube = () => {
   return (
     <div className="lego-youtube-section py-5 my-5">
-      <h2 className="section-title text-center">Lego YouTube</h2>
-      <div className="video-container d-flex align-items-center justify-content-center">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/fqXQBLoLIH0?start=0"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-      </div>
-      
+      <Container>
+        <h2 className="section-title text-center">Lego YouTube</h2>
+        <div className="video-container d-flex align-items-center justify-content-center">
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/fqXQBLoLIH0?start=0"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </Container>
     </div>
   );
 };
